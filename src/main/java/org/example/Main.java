5f700d98910804cfb7457bd25d42e5009f151f48
yu.sugrobov <yu.sugrobov@itfbgroup.ru>
2024-04-12 11:04:30 +0300

    qwe

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,7 +4,11 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("added develop branch" +
-                "");
+        System.out.println("added develop branch");
+
+
+        for (char c : "qweqweq".toCharArray()) {
+            System.out.println(c);
+        }
     }
 }
