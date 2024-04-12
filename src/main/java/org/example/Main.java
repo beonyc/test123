d5880f1a1db0740bc8c49be0c955314a0435a3ec
yu.sugrobov <yu.sugrobov@itfbgroup.ru>
2024-04-12 12:18:15 +0300

    dev

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,17 +3,7 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        //Данные из develop
-
-        System.out.println("added develop branch");
 
 
-        for (char c : "qweqweq".toCharArray()) {
-            System.out.println(c);
-        }
-        //я исправил ошибку
-        System.out.println(Data.DATA.getLogin()+" "+Data.DATA.getPassword());
-
-        //данные из Developp_copy
     }
 }
